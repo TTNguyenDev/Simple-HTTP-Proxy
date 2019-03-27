35bdd75b3930585e8463289f5a5563a5aa9bfc86
TTNguyen <nttriet9a4@gmail.com>
2019-03-27 20:59:25 +0700

    - Update blacklist.conf - Handled checking safe url

--- httpProxy/src/MyProxyServer.java
+++ httpProxy/src/MyProxyServer.java
@@ -1,12 +1,9 @@
 
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +15,32 @@ public class MyProxyServer {
         MyProxyServer myProxyServer = new MyProxyServer();
         myProxyServer.start();
     }
+
+
+    //return true when url exist in blacklist else return false
+    public boolean checkBlacklist(String mUrl) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("/Users/trietnguyen/Desktop/Simple-HTTP-Proxy/httpProxy/blacklist.conf"));
+
+            String textInALine;
+
+            while ((textInALine = br.readLine()) != null) {
+                if (mUrl.equals(textInALine))
+                    return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
     public void start() {
         System.out.println("Starting the MyProxyServer ...");
         try {
@@ -25,9 +48,9 @@ public class MyProxyServer {
             ServerSocket serverSocket = new ServerSocket(MyProxyServer.portNumber, 1);
 
             while (true) {
-                System.out.println("Inside while loop ");
+                //System.out.println("Inside while loop ");
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Connection to MyProxyServer is " + clientSocket.isConnected());
+                //System.out.println("Connection to MyProxyServer is " + clientSocket.isConnected());
 
                 InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
 
@@ -36,17 +59,21 @@ public class MyProxyServer {
 
                 bufferedReader.close();
                 
-                
-                
-                //Handled check blacklist
+                //---------Handled check blacklist
                 String[] words = command.split("\\s");
                 String mURL = words[1];
 
+
+
                 System.out.println(mURL);
-                
+                if (checkBlacklist(mURL)) {
+                    System.out.println(mURL + " đã tồn tại trong blacklist");
+                } else {
+                    System.out.println("Được phép truy cập");
+                }
                 
 
-                System.out.println("Client has asked to ....\n" + command);
+               // System.out.println("Client has asked to ....\n" + command);
 
                 if (command.equals("Cancel")) {
                     System.out.println("Shutting down the server ...");
