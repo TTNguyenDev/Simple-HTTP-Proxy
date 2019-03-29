a61ace6eec8d36f44a06f5bbf84dd6a5b223c1a1
TTNguyen <nttriet9a4@gmail.com>
2019-03-29 10:49:36 +0700

    - update

--- httpProxy/src/MyProxyServer.java
+++ httpProxy/src/MyProxyServer.java
@@ -1,17 +1,14 @@
-
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.sql.SQLOutput;
 
 public class MyProxyServer {
 
+    private final String USER_AGENT = "Mozilla/5.0";
+
     public static final int portNumber = 8888;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         MyProxyServer myProxyServer = new MyProxyServer();
         myProxyServer.start();
     }
@@ -41,7 +38,39 @@ public class MyProxyServer {
         return false;
     }
 
-    public void start() {
+    private void sendGet(String url, OutputStream destination) throws Exception {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine).append("\r\n");
+        }
+        in.close();
+        destination.write(response.toString().getBytes());
+        destination.flush();
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    public void start() throws Exception {
         System.out.println("Starting the MyProxyServer ...");
         try {
 
@@ -52,19 +81,52 @@ public class MyProxyServer {
                 InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
 
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String requestFromClient;
-                int i = 0;
-                while (i<8) {
-                    requestFromClient = bufferedReader.readLine();
-                    System.out.println(requestFromClient);
-                    i++;
-                }
-                bufferedReader.close();
+                String line;
+                StringBuffer request = new StringBuffer();
+                String mURL;
+                line = bufferedReader.readLine();
+                request.append(line).append("\r\n");
+                String[] requestLineComponents = line.split(" ");
+                mURL = requestLineComponents[1];
 
-                //---------Handled check blacklist
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (line.equals(""))
+                        break;
+                    request.append(line).append("\r\n");
+                }
+
+
+
+                System.out.println(request.toString() + "\n" + "\n");
+                String realUrl = mURL.split("://")[1];
+                realUrl = realUrl.substring(0, realUrl.length() - 1);
+                sendGet(mURL, clientSocket.getOutputStream());
+//                InetAddress address = InetAddress.getByName(realUrl);
+//                Socket desSocket = new Socket(address, 64122);
+//                desSocket.getOutputStream().write(request.toString().getBytes());
+//
+//                BufferedReader resReader = new BufferedReader(new InputStreamReader(desSocket.getInputStream()));
+//                StringBuilder response = new StringBuilder();
+//                while (true) {
+//                    line = resReader.readLine();
+//                    if (line.equals(""))
+//                        break;
+//
+//                    response.append(line).append("\r\n");
+//                }
+//
+//                System.out.println(response);
+                //while ((requestFromClient = bufferedReader.readLine()) != null) {
+                //    System.out.println(requestFromClient);
+                //}
+                //inputStreamReader.close();
+
+
+//                sendGet(mURL, clientSocket.getOutputStream());
+//                ---------Handled check blacklist
 //                String[] words = requestFromClient.split("\\s");
 //                String mURL = words[1];
-
+                //clientSocket.close();
 
 
 //                System.out.println(mURL);
@@ -79,6 +141,7 @@ public class MyProxyServer {
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
+            System.out.println(e.toString());
             e.printStackTrace();
         }
     }
