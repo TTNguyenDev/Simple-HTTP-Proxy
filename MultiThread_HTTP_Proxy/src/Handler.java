f88724d0d150b24f8a1f4786f57cec9705154e03
TTNguyen <nttriet9a4@gmail.com>
2019-04-10 14:20:55 +0700

    Project Completed

--- MultiThread_HTTP_Proxy/src/Handler.java
+++ MultiThread_HTTP_Proxy/src/Handler.java
@@ -13,10 +13,32 @@ public class Handler extends Thread {
         this.mClientSocket = socket;
     }
 
+    public boolean checkBlacklist(String mUrl) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("/Users/trietnguyen/Desktop/Simple-HTTP-Proxy/MultiThread_HTTP_Proxy/blacklist.conf"));
+
+            String textInALine;
+
+            while ((textInALine = br.readLine()) != null) {
+                if (mUrl.equals(textInALine))
+                    return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
     public void run() {
         try {
-            while (true) {
-
+            while(true) {
                 //get Request from browser
                 BufferedReader mClientRequest = new BufferedReader(new InputStreamReader(mClientSocket.getInputStream()));
                 String line;
@@ -37,27 +59,63 @@ public class Handler extends Thread {
                 URL mUrl = new URL(url);
                 String mHost = mUrl.getHost();
 
-                //print to console Domain name & http request
-                System.out.println("URL: " + url);
-                System.out.println(mClientRequestMessage);
+                if (checkBlacklist(mHost)) {
+                    DataOutputStream mResponseToClient = new DataOutputStream(mClientSocket.getOutputStream());
+                    String line2 = "HTTP/1.1 403 Access Forbidden\r\n\r\n" + "<!DOCTYPE html>\n" +
+                            "<html lang=\"en\">\n" +
+                            "<head>\n" +
+                            "    <meta charset=\"UTF-8\">\n" +
+                            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                            "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" +
+                            "    <title>403 Access Forbidden</title>\n" +
+                            "    <style> \n" +
+                            "        .centered {\n" +
+                            "            position: fixed;\n" +
+                            "            top: 50%;\n" +
+                            "            left: 50%;\n" +
+                            "            color: red;\n" +
+                            "            font-size: 80px;\n" +
+                            "            font-weight: 700;\n" +
+                            "            /* bring your own prefixes */\n" +
+                            "            transform: translate(-50%, -50%);\n" +
+                            "        }    \n" +
+                            "    </style>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "    <div class=\"centered\">\n" +
+                            "        403 Access Forbidden\n" +
+                            "      </div>\n" +
+                            "</body>\n" +
+                            "</html>";
 
-                //forward request to server
-                Socket mServerSocket = new Socket(InetAddress.getByName(mHost), 80);
-                DataOutputStream mForwardMessageWriter = new DataOutputStream((mServerSocket.getOutputStream()));
-                mForwardMessageWriter.writeBytes(mClientRequestMessage.toString());
-                mForwardMessageWriter.flush();
-
-                //get response message from server
-                InputStream mResponseFromServer = mServerSocket.getInputStream();
-                DataOutputStream mResponseToClient = new DataOutputStream(mClientSocket.getOutputStream());
-                //begin send response to client
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int read = 0;
-                while ((read = mResponseFromServer.read(buffer)) > 0) {
-                    mResponseToClient.write(buffer, 0, read);
+                    mResponseToClient.writeBytes(line2.toString());
                     mResponseToClient.flush();
+                    mClientSocket.close();
+
+                } else {
+//                print to console Domain name & http request
+                    System.out.println("URL: " + url);
+                    System.out.println(mClientRequestMessage);
+
+                    //forward request to server
+                    Socket mServerSocket = new Socket(InetAddress.getByName(mHost), 80);
+                    DataOutputStream mForwardMessageWriter = new DataOutputStream((mServerSocket.getOutputStream()));
+                    mForwardMessageWriter.writeBytes(mClientRequestMessage.toString());
+                    mForwardMessageWriter.flush();
+
+                    //get response message from server
+                    InputStream mResponseFromServer = mServerSocket.getInputStream();
+                    DataOutputStream mResponseToClient = new DataOutputStream(mClientSocket.getOutputStream());
+                    //begin send response to client
+                    byte[] buffer = new byte[BUFFER_SIZE];
+                    int read = 0;
+                    while ((read = mResponseFromServer.read(buffer)) > 0) {
+                        mResponseToClient.write(buffer, 0, read);
+                        mResponseToClient.flush();
+                    }
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
